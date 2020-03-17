5fdedcdff5a06e021cad7a66106bb40fe424fca4
Ari Mendelow <ari@mendelow.me>
2020-03-17 12:54:08 -0700

    Script now captures all job alerts, navigates to them, and sorts by most recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -19,3 +20,30 @@ password.send_keys(Keys.RETURN)
 #   driver.find_element_by_xpath("//span[contains(@id,'prompt')]//following::button").click()
 
 driver.get("https://www.linkedin.com/jobs/")
+
+driver.find_element_by_xpath("//button[contains(@aria-label,'job alerts')]").click()
+
+
+alert_list = driver.find_element_by_xpath("//h3[contains(text(), 'Saved job alerts')]//following::ul")
+
+# Sometimes takes it a bit to load the list
+time.sleep(2)
+
+alerts = alert_list.find_elements_by_xpath(".//li")
+
+alert_links = []
+for alert in alerts:
+  link = alert.find_element_by_xpath(".//a")
+  alert_links.append(link.get_attribute("href"))
+
+for job_search in alert_links:
+  driver.get(job_search)
+
+  # Sort by recent
+  driver.find_element_by_xpath("//artdeco-dropdown-trigger[contains(@aria-label, 'Sort by')]").click()
+  driver.find_element_by_xpath("//input[contains(@id, 'sort-by-date')]//parent::*").click()
+  driver.find_element_by_xpath("//artdeco-dropdown-trigger[contains(@aria-label, 'Sort by')]").click()
+
+  
+
+
